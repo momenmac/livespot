1b95100523a5f1a37ba677838665d6399540464c
Nopo69420 <toostronkm@gmail.com>
2025-02-22 21:52:12 +0200

    Map dependcies show map

--- lib/ui/pages/map_page.dart
+++ lib/ui/pages/map_page.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final MapController _mapController = MapController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Map'),
+        foregroundColor: Colors.white,
+        title: const Text('Map'),
+        backgroundColor: Colors.blue,
       ),
-      body: WebviewScaffold(
-        url: 'assets/map.html',
-        withZoom: true,
-        withLocalStorage: true,
-        hidden: true,
-        initialChild: Center(
-          child: CircularProgressIndicator(),
-        ),
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: LatLng(0, 0),
+              initialZoom: 2,
+              minZoom: 0,
+              maxZoom: 100,
+            ),
+            children: [
+              TileLayer(
+                  urlTemplate:
+                      "https://tile.openstreetmap.org/{z}/{x}/{y}.png"),
+            ],
+          ),
+        ],
       ),
     );
   }
